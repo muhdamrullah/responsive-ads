9286c793bade8ceb7a5c959bb89d39dbe7da5551
muhdamrullah <muneorion@gmail.com>
2016-04-08 23:20:53 +0800

    Update loop.js

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,7 +1,7 @@
 $(function(){
 
 var picCount=0;  
-var picArray= ["http://bit.ly/1uxYSSb","http://bit.ly/1oOrsO6","http://design.ubuntu.com/wp-content/uploads/ubuntu-logo32.png","https://media.giphy.com/media/xTiQymeVWS8KUMeSpa/giphy.gif"];
+var picArray= ["http://bit.ly/1uxYSSb","http://bit.ly/1oOrsO6","http://design.ubuntu.com/wp-content/uploads/ubuntu-logo32.png","https://media.giphy.com/media/3oGRFE0qeWw4CLsP7O/giphy.gif"];
 nextPic();
 
 function nextPic() { 
