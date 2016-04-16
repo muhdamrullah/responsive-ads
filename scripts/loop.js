b9627b79ce856cbd1457ab5248e92bd7bc27ae4e
muhdamrullah <muneorion@gmail.com>
2016-04-16 17:13:10 +0800

    Updated gif

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,7 +1,12 @@
 $(function(){
 
 var picCount=0;  
-var picArray= ["https://media.giphy.com/media/l4hLy5WvoC7mA6vPq/giphy.gif","https://media.giphy.com/media/26CYzLeA6jmwetE9W/giphy.gif","http://i.giphy.com/xT1XGUd5IT0dwQLJKw.gif","https://media.giphy.com/media/l0LIX7gSPKtg8oHT2/giphy.gif"];
+var picArray= [
+   'https://media.giphy.com/media/l4hLy5WvoC7mA6vPq/giphy.gif',
+   'https://media.giphy.com/media/26CYzLeA6jmwetE9W/giphy.gif',
+   'https://media.giphy.com/media/3o85fVTFUtaFUhB1Ze/giphy.gif',
+   "http://i.giphy.com/xT1XGUd5IT0dwQLJKw.gif",
+];
 nextPic();
 
 function nextPic() { 
